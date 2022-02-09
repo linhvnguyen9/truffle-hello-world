772a703acc2bdbcd85709507017d18b66479130c
Van Linh Nguyen <linhvnguyen9@gmail.com>
2022-02-09 20:55:26 +0700

    add tx.origin demo

--- contracts/HelloWorld2.sol
+++ contracts/HelloWorld2.sol
@@ -1,17 +1,11 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 // Modified Greeter contract. Based on example at https://www.ethereum.org/greeter.
-
-import "./HelloWorld2.sol";
-
-contract HelloWorld {
+contract HelloWorld2 {
     string greet;
-
-    HelloWorld2 anotherContract;
 
     constructor (string memory _greet) {
         greet = _greet;
-        anotherContract = new HelloWorld2(_greet);
     }
 
     function greeting() public view returns (string memory) {
@@ -20,9 +14,5 @@ contract HelloWorld {
 
     function test() public view returns (address) {
         return tx.origin;
-    }
-
-    function test2() public view returns (address) {
-        return anotherContract.test();
     }
 }
